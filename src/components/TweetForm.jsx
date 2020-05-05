1b6f0774a4fd2c18226d482bfe34646ed4a005cd
Alex Bloom <lehanbloom@gmail.com>
2020-05-05 16:15:36 +0300

    Milestone 1. Added alert / button disable when tweet is over 140 characters.

--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -1,26 +1,50 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import "../css/TweetForm.css";
 
 class TweetForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { displayAlert: false, tweet: "" };
+  }
+
+  handleUserTypingTweet(event) {
+    let tweet = event.target.value;
+    if (tweet.length > 5) {
+      this.setState({ displayAlert: true });
+    } else {
+      this.setState({ displayAlert: false, tweet });
+    }
   }
 
   render() {
+    let { displayAlert } = this.state;
     return (
       <Form>
         <Form.Group controlId="formBasicEmail" className="tweet-form">
-          <textarea placeholder="What's on your mind..."></textarea>
-          {/* <input
-            type="text"
-            placeholder="What's on your mind"
-            className="tweet-input-field"
-          ></input> */}
-          <Button variant="primary" type="submit">
-            Tweet
-          </Button>
+          <div className="main-content">
+            <textarea
+              placeholder="What's on your mind..."
+              onChange={(event) => {
+                this.handleUserTypingTweet(event);
+              }}
+            ></textarea>
+            {!displayAlert && (
+              <Button variant="primary" type="submit">
+                Tweet
+              </Button>
+            )}
+            {displayAlert && (
+              <Button variant="primary" type="submit" disabled>
+                Tweet
+              </Button>
+            )}
+          </div>
+          {displayAlert && (
+            <Alert variant="danger">
+              Tweet can't be more than 140 characters
+            </Alert>
+          )}
         </Form.Group>
       </Form>
     );
